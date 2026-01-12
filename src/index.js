3f60426378685b6935b2363433b3eaedc59ceb02
Voltranger <pranav.101205mahajan@gmail.com>
2026-01-12 22:27:37 +0530

    new changes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import DB_NAME  from "./constants.js";
+
+import express from "express";
+const app=express();
+
+
+(async()=>{
+    try
+    {
+        await mongoose.connect( `${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(err)=>{throw err;})
+    
+    
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server started at PORT ${process.env.PORT}`);
+        });
+    }
+     
+    catch(err)
+    {
+        console.error("Error:",err);
+        throw(err);
+    }
+})()//IIFE:IMMEDIATELY INVOKED FUNCTION EXPRESSION
