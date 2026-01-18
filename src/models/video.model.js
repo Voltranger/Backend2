cc2036f6f5926945dad5bc2d51ceedba01a3a609
Voltranger <pranav.101205mahajan@gmail.com>
2026-01-18 14:02:42 +0530

    models

---
+++ src/models/video.model.js
@@ -0,0 +1,52 @@
+import mongoose,{Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+const VideoSchema=new Schema(
+    {
+        videoFile:{
+            type:String,//cloudinary url
+            required:true,
+
+        },
+        thumbnail:
+        {
+            type:String,//cloudinary url
+            required:true
+        },
+        description:
+        {
+            type:String,
+            required:true
+        },
+        title:
+        {
+            type:String,
+            required:true
+        },
+        duration:
+        {
+            type:Number,
+            required:true
+        },
+
+        views:{
+            tupe:Number,
+            default:0
+        },
+
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
+
+    },
+    {timestamps:true}
+)
+
+
+
+export const Video=mongoose.model("Video",VideoSchema)
