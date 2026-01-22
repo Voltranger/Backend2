cd91ad63548d14eaacc6264a6fcab4bb195c5af8
Voltranger <pranav.101205mahajan@gmail.com>
2026-01-22 21:56:00 +0530

    ADDED USER MODEL

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema=new Schema(
             
 
         },
-        fullname:{
+        fullName:{
             type:String,
             required:true,
             unique:true,
@@ -59,8 +59,54 @@ const userSchema=new Schema(
 )
 
 userSchema.pre("save",async function(next){  
-     
+     if(this.isModified("password")) return next();
     this.password=bcrypt.hash(this.password,10)//rounds=10
     next()
 })
+
+
+userSchema.methods.isPasswordCorrect=async function(password)
+{
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function()
+{
+    return jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:fullName
+        },
+
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+
+    )
+
+}
+
+
+userSchema.methods.generateRefreshToken=function()
+{
+        return jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+
+}
+
+
 export const User=mongoose.model("User",userSchema)
