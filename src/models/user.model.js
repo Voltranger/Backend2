70cdb608f50d9befe297d2bc8d7ef6690a257eb7
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-07 18:36:07 +0530

    user register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,8 +59,10 @@ const userSchema=new Schema(
 )
 
 userSchema.pre("save",async function(next){  
-     if(this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)//rounds=10
+    if(this.isModified("password")) return next();
+    
+    this.password=await bcrypt.hash(this.password,10)//rounds=10
+
     next()
 })
 
