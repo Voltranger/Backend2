fde4f1a902530aa73de94bd35b6defb808d87fc4
Voltranger <pranav.101205mahajan@gmail.com>
2026-02-08 15:35:44 +0530

    Refactor user model password hashing logic in pre-save hook for improved clarity and functionality.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,12 +59,9 @@ const userSchema=new Schema(
     }
 )
 
-userSchema.pre("save",async function(next){  
-    if(this.isModified("password")) return next();
-    
-    this.password=await bcrypt.hash(this.password,10)//rounds=10
-
-    next()
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 
